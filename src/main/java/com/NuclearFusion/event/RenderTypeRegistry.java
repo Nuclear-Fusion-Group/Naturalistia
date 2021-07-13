7d0ad07c8ecd1640e24e2b067fb406c2bfa3f61c
skyinr <2587135207@qq.com>
2021-07-13 21:22:52 +0800

    作物

---
+++ src/main/java/com/NuclearFusion/event/RenderTypeRegistry.java
@@ -0,0 +1,28 @@
+package com.NuclearFusion.event;
+
+import com.NuclearFusion.Naturalistia;
+import com.NuclearFusion.block.BlockRegistry;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD,modid = Naturalistia.MOD_ID)
+public class RenderTypeRegistry {
+    @SubscribeEvent
+    public static void onRenderTypeSetup(FMLClientSetupEvent event){
+        event.enqueueWork(()->{
+            RenderTypeLookup.setRenderLayer(BlockRegistry.BLOCK_BELLADONNA.get(), RenderType.getCutout());
+        });
+        event.enqueueWork(()->{
+            RenderTypeLookup.setRenderLayer(BlockRegistry.BLOCK_CHILIPEPPER.get(), RenderType.getCutout());
+        });
+        event.enqueueWork(()->{
+            RenderTypeLookup.setRenderLayer(BlockRegistry.BLOCK_DATURA.get(), RenderType.getCutout());
+        });
+        event.enqueueWork(()->{
+            RenderTypeLookup.setRenderLayer(BlockRegistry.BLOCK_SKYFAERY.get(), RenderType.getCutout());
+        });
+    }
+}
