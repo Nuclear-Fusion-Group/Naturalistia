c13c44adbfe5098e3d3923aca5fed620d173f42a
TT432 <2437315224@qq.com>
2021-07-17 16:38:00 +0800

    效果修复

--- src/main/java/com/NuclearFusion/Effect/EffectRegister.java
+++ src/main/java/com/NuclearFusion/Effect/EffectRegister.java
@@ -14,8 +14,8 @@ public class EffectRegister {
     public static final DeferredRegister<Effect> Effect = DeferredRegister.create(ForgeRegistries.POTIONS, Naturalistia.MOD_ID);
 
     public static final RegistryObject<Effect> EFFECT_VERTIGO = Effect.register("effect_vertigo", () -> new OBJvertigo()
-            .addAttributesModifier(Attributes.ATTACK_DAMAGE, "22653B89-116E-49DC-9B6B-9971489B5BE5", 0, AttributeModifier.Operation.MULTIPLY_TOTAL)
-            .addAttributesModifier(Attributes.MOVEMENT_SPEED, "7107DE5E-7CE8-4030-940E-514C1F160890", 0, AttributeModifier.Operation.MULTIPLY_TOTAL)
-            .addAttributesModifier(Attributes.ATTACK_SPEED, "55FCED67-E92A-486E-9800-B47F202C4386",0,AttributeModifier.Operation.MULTIPLY_TOTAL));
+            .addAttributesModifier(Attributes.ATTACK_DAMAGE,  "5a64a413-cdc5-d80b-e4b8-8f09ad67c62c", 0, AttributeModifier.Operation.MULTIPLY_TOTAL)
+            .addAttributesModifier(Attributes.MOVEMENT_SPEED, "5739c3c3-f93d-02d5-53f8-37a74d61e7dd", 0, AttributeModifier.Operation.MULTIPLY_TOTAL)
+            .addAttributesModifier(Attributes.ATTACK_SPEED,   "162f964b-2c59-2843-b6ee-69f060dcc6e7",0,AttributeModifier.Operation.MULTIPLY_TOTAL));
 
 }
