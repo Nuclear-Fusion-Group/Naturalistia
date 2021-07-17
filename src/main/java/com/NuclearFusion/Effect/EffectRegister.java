3a3da21a0f53868fad1812b6b3e64845d6bb9596
TT432 <2437315224@qq.com>
2021-07-17 16:22:13 +0800

    “眩晕”效果修复

--- src/main/java/com/NuclearFusion/Effect/EffectRegister.java
+++ src/main/java/com/NuclearFusion/Effect/EffectRegister.java
@@ -14,8 +14,8 @@ public class EffectRegister {
     public static final DeferredRegister<Effect> Effect = DeferredRegister.create(ForgeRegistries.POTIONS, Naturalistia.MOD_ID);
 
     public static final RegistryObject<Effect> EFFECT_VERTIGO = Effect.register("effect_vertigo", () -> new OBJvertigo()
-            .addAttributesModifier(Attributes.ATTACK_DAMAGE, UUID.randomUUID().toString(), 0.0D, AttributeModifier.Operation.ADDITION)
-            .addAttributesModifier(Attributes.MOVEMENT_SPEED, UUID.randomUUID().toString(), -1.0D, AttributeModifier.Operation.ADDITION)
-            .addAttributesModifier(Attributes.ATTACK_SPEED, UUID.randomUUID().toString(),-1.0D,AttributeModifier.Operation.ADDITION));
+            .addAttributesModifier(Attributes.ATTACK_DAMAGE, "22653B89-116E-49DC-9B6B-9971489B5BE5", 0, AttributeModifier.Operation.MULTIPLY_TOTAL)
+            .addAttributesModifier(Attributes.MOVEMENT_SPEED, "7107DE5E-7CE8-4030-940E-514C1F160890", 0, AttributeModifier.Operation.MULTIPLY_TOTAL)
+            .addAttributesModifier(Attributes.ATTACK_SPEED, "55FCED67-E92A-486E-9800-B47F202C4386",0,AttributeModifier.Operation.MULTIPLY_TOTAL));
 
 }
