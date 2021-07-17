e83e29fee8ee2e6adc3cc9a4676d98867b3f84cb
枫叶秋林 <liuqianpan2018@outlook.com>
2021-07-17 09:58:16 +0800

    增加武器描述

--- src/main/java/com/NuclearFusion/item/arms/sword/NimlosilverSword.java
+++ src/main/java/com/NuclearFusion/item/arms/sword/NimlosilverSword.java
@@ -2,6 +2,9 @@ package com.NuclearFusion.item.arms.sword;
 
 import com.NuclearFusion.api.ModItemTier;
 import com.NuclearFusion.item.ItemRegister;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.client.util.InputMappings;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.CreatureAttribute;
 import net.minecraft.entity.Entity;
@@ -13,6 +16,12 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.item.SwordItem;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.SoundEvents;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
+import org.lwjgl.glfw.GLFW;
+
+import java.util.List;
 
 public class NimlosilverSword extends SwordItem {
 
@@ -21,8 +30,16 @@ public class NimlosilverSword extends SwordItem {
     }
 
     @Override
-    public boolean hitEntity(ItemStack stack, LivingEntity target, LivingEntity attacker) {
-        target.attackEntityFrom(DamageSource.DRAGON_BREATH, EnchantmentHelper.getSweepingDamageRatio(attacker) * 5);
-        return super.hitEntity(stack, target, attacker);
+    public void addInformation(ItemStack stack, World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+        super.addInformation(stack, worldIn, tooltip, flagIn);
+        //.getWindow().getWindow();
+        long WINDOW = Minecraft.getInstance().getMainWindow().getHandle();
+        tooltip.add(new TranslationTextComponent("message.Sword.describe"));
+        if (InputMappings.isKeyDown(WINDOW, GLFW.GLFW_KEY_LEFT_SHIFT) || InputMappings.isKeyDown(WINDOW, GLFW.GLFW_KEY_RIGHT_SHIFT)) {
+            tooltip.add(new TranslationTextComponent("message.Release_shift"));
+            tooltip.add(new TranslationTextComponent("message.Sword.info"));
+        }else {
+            tooltip.add(new TranslationTextComponent("message.Press_shift"));
+        }
     }
 }
