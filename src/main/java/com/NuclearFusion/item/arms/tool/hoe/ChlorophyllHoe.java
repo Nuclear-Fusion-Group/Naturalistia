2788a2e8206d198946d29b33f8fd0aa6b36f814b
skyinr <2587135207@qq.com>
2021-07-29 10:55:55 +0800

    bug修复

--- src/main/java/com/NuclearFusion/item/arms/tool/hoe/ChlorophyllHoe.java
+++ src/main/java/com/NuclearFusion/item/arms/tool/hoe/ChlorophyllHoe.java
@@ -3,10 +3,11 @@ package com.NuclearFusion.item.arms.tool.hoe;
 import com.NuclearFusion.api.ModItemTier;
 import com.NuclearFusion.item.ItemRegister;
 import com.NuclearFusion.item.arms.tool.NaturalistiaShovel;
+import net.minecraft.item.SwordItem;
 
-public class ChlorophyllHoe extends NaturalistiaShovel {
+public class ChlorophyllHoe extends SwordItem {
     public ChlorophyllHoe() {
 
-        super(1.5F, -3.0F, new ModItemTier(3, 300, 7.5F, 5.5F, 30, ItemRegister.ITEM_INGOT_CHLOROPHYLL.get()), 3, ItemRegister.defaultBuilder());
+        super(new ModItemTier(3, 300, 7.5F, 5.5F, 30, ItemRegister.ITEM_INGOT_CHLOROPHYLL.get()), 3, -3.0F, ItemRegister.defaultBuilder());
     }
 }
