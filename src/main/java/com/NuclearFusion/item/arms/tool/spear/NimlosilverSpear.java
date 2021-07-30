6bf153ce8567d47f7b46cff8e428c874346dc9a6
skyinr <2587135207@qq.com>
2021-07-30 19:23:28 +0800

    编码

--- src/main/java/com/NuclearFusion/item/arms/tool/spear/NimlosilverSpear.java
+++ src/main/java/com/NuclearFusion/item/arms/tool/spear/NimlosilverSpear.java
@@ -3,13 +3,10 @@ package com.NuclearFusion.item.arms.tool.spear;
 import com.NuclearFusion.api.ModItemTier;
 import com.NuclearFusion.api.NimlosilverBuff;
 import com.NuclearFusion.item.ItemRegister;
-import com.NuclearFusion.item.arms.tool.NaturalistiaShovel;
-import net.minecraft.item.SwordItem;
-import net.minecraft.item.ToolItem;
 
 public class NimlosilverSpear extends NimlosilverBuff {
     public NimlosilverSpear() {
         super(new ModItemTier(3, 250, 7.5F, 5.5F, 30, ItemRegister.ITEM_INGOT_NIMLOSILVER.get()), 3, -2.8F, ItemRegister.defaultBuilder());
     }
-    //TODO 1.5±∂π•ª˜æ‡¿Î
+    //TODO 1.5ÂÄçÊîªÂáªË∑ùÁ¶ª
 }
