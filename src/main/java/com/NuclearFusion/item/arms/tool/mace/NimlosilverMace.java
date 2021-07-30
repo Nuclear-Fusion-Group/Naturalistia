26fe96239ca088a2c7a710519de3b7054826a269
TT432 <2437315224@qq.com>
2021-07-30 21:07:47 +0800

    add 制作后附魔

--- src/main/java/com/NuclearFusion/item/arms/tool/mace/NimlosilverMace.java
+++ src/main/java/com/NuclearFusion/item/arms/tool/mace/NimlosilverMace.java
@@ -6,6 +6,7 @@ import com.NuclearFusion.api.NimlosilverBuff;
 import com.NuclearFusion.item.ItemRegister;
 import net.minecraft.enchantment.EnchantmentData;
 import net.minecraft.enchantment.Enchantments;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
@@ -13,6 +14,7 @@ import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.nbt.ListNBT;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.world.World;
 
 public class NimlosilverMace extends NimlosilverBuff {
     public NimlosilverMace() {
@@ -24,20 +26,14 @@ public class NimlosilverMace extends NimlosilverBuff {
     public void fillItemGroup(ItemGroup group, NonNullList<ItemStack> items) {
         if (group == ItemRegister.creativeTab) {
             ItemStack itemstack = this.getItem().getDefaultInstance();
-
-            CompoundNBT nbt = new CompoundNBT();
-            ListNBT listNBT = new ListNBT();
-            EnchantmentData stackIn = new EnchantmentData(Enchantments.KNOCKBACK, 1);
-
-            //添加附魔与等级
-            nbt.putString("id", String.valueOf(Registry.ENCHANTMENT.getKey(stackIn.enchantment)));
-            nbt.putShort("lvl", (short) stackIn.enchantmentLevel);
-
-            listNBT.add(nbt);
-            itemstack.getOrCreateTag().put("Enchantments", listNBT);
-
+            itemstack.addEnchantment(Enchantments.KNOCKBACK, 1);
             items.add(itemstack);
         }
     }
 
+    @Override
+    public void onCreated(ItemStack p_77622_1_, World p_77622_2_, PlayerEntity p_77622_3_) {
+        p_77622_1_.addEnchantment(Enchantments.KNOCKBACK, 1);
+        super.onCreated(p_77622_1_, p_77622_2_, p_77622_3_);
+    }
 }
