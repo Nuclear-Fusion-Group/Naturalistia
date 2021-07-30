9bb98ca110bb81346e674d11fb59d166925e4915
skyinr <2587135207@qq.com>
2021-07-30 16:07:02 +0800

    使物品自带附魔

--- src/main/java/com/NuclearFusion/item/arms/tool/mace/NimlosilverMace.java
+++ src/main/java/com/NuclearFusion/item/arms/tool/mace/NimlosilverMace.java
@@ -4,12 +4,41 @@ package com.NuclearFusion.item.arms.tool.mace;
 import com.NuclearFusion.api.ModItemTier;
 import com.NuclearFusion.api.NimlosilverBuff;
 import com.NuclearFusion.item.ItemRegister;
-import com.NuclearFusion.item.arms.tool.NaturalistiaShovel;
-import net.minecraft.item.SwordItem;
+import net.minecraft.enchantment.EnchantmentData;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.nbt.ListNBT;
+import net.minecraft.util.NonNullList;
+import net.minecraft.util.registry.Registry;
 
 public class NimlosilverMace extends NimlosilverBuff {
     public NimlosilverMace() {
-        super(new ModItemTier(3, 300, 7.5F, 5.5F, 30, ItemRegister.ITEM_INGOT_NIMLOSILVER.get()), 3, -3.0F, ItemRegister.defaultBuilder());
+        super(new ModItemTier(3, 300, 7.5F, 5.5F, 30, ItemRegister.ITEM_INGOT_NIMLOSILVER.get()), 3, -3.0F, new Item.Properties());
     }
-    //TODO 自带击退1
+
+    //TODO 如果后面需要自带附魔的物品很多的话 可以考虑将功能独立出去
+    @Override
+    public void fillItemGroup(ItemGroup group, NonNullList<ItemStack> items) {
+        if (group == ItemRegister.creativeTab) {
+            ItemStack itemstack = this.getItem().getDefaultInstance();
+
+            CompoundNBT nbt = new CompoundNBT();
+            ListNBT listNBT = new ListNBT();
+            EnchantmentData stackIn = new EnchantmentData(Enchantments.KNOCKBACK, 1);
+
+            //添加附魔与等级
+            nbt.putString("id", String.valueOf(Registry.ENCHANTMENT.getKey(stackIn.enchantment)));
+            nbt.putShort("lvl", (short) stackIn.enchantmentLevel);
+
+            listNBT.add(nbt);
+            itemstack.getOrCreateTag().put("Enchantments", listNBT);
+
+            items.add(itemstack);
+        }
+        super.fillItemGroup(group, items);
+    }
+
 }
