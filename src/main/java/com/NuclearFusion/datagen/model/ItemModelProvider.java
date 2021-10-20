41d4e879442b0f3611b5cc233ac5502be1214949
skyinr <2587135207@qq.com>
2021-10-20 13:34:14 +0800

    小修补

--- src/main/java/com/NuclearFusion/datagen/model/ItemModelProvider.java
+++ src/main/java/com/NuclearFusion/datagen/model/ItemModelProvider.java
@@ -1,9 +1,8 @@
 package com.NuclearFusion.datagen.model;
 
 import com.NuclearFusion.Naturalistia;
-import com.NuclearFusion.block.BlockRegistry;
+import com.NuclearFusion.block.crop.Shrub;
 import net.minecraft.data.DataGenerator;
-import net.minecraft.data.DirectoryCache;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.CrossbowItem;
 import net.minecraft.item.Item;
@@ -20,14 +19,15 @@ import static com.NuclearFusion.api.ResourceLocationHelper.prefix;
 
 //此处借鉴了植物魔法
 public class ItemModelProvider extends net.minecraftforge.client.model.generators.ItemModelProvider {
+    private static final ResourceLocation GENERATED = new ResourceLocation("item/generated");
+    private static final ResourceLocation HANDHELD = new ResourceLocation("item/handheld");
 
     public ItemModelProvider(DataGenerator generator, ExistingFileHelper existingFileHelper) {
         super(generator, Naturalistia.MOD_ID, existingFileHelper);
     }
 
-    @Override
-    protected void generateAll(DirectoryCache cache) {
-        super.generateAll(cache);
+    private static String name(Item i) {
+        return Registry.ITEM.getKey(i).getPath();
     }
 
     @Override
@@ -38,9 +38,6 @@ public class ItemModelProvider extends net.minecraftforge.client.model.generator
         registerItemBlock(items.stream().filter(i -> i instanceof BlockItem).map(i -> (BlockItem) i).collect(Collectors.toSet()));
         registerItem(items);
     }
-
-    private static final ResourceLocation GENERATED = new ResourceLocation("item/generated");
-    private static final ResourceLocation HANDHELD = new ResourceLocation("item/handheld");
 
     private ItemModelBuilder handheldItem(String name) {
         return withExistingParent(name, HANDHELD)
@@ -60,16 +57,11 @@ public class ItemModelProvider extends net.minecraftforge.client.model.generator
         return generatedItem(name(i));
     }
 
-    private static String name(Item i) {
-        return Registry.ITEM.getKey(i).getPath();
-    }
-
     private void registerItemBlock(Set<BlockItem> itemBlocks) {
-        //TODO 这里没想到一个优雅的解决方案
-        itemBlocks.remove(BlockRegistry.BLOCK_BELLADONNA.get().asItem());
-        itemBlocks.remove(BlockRegistry.BLOCK_CHILIPEPPER.get().asItem());
-        itemBlocks.remove(BlockRegistry.BLOCK_DATURA.get().asItem());
-        itemBlocks.remove(BlockRegistry.BLOCK_SKYFAERY.get().asItem());
+        //清除一些不想自动注册的模型
+        itemBlocks.removeAll(itemBlocks.stream()
+                .filter(i -> i.getBlock() instanceof Shrub)
+                .collect(Collectors.toSet()));
 
         itemBlocks.forEach(i -> {
             String name = Registry.ITEM.getKey(i).getPath();
